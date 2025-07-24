5a351e0cfd1fd026563cb3ec942ff58a2fd962fe
Aryaman Sriram <aryamansriram842@gmail.com>
2025-07-23 21:39:44 -0400

    Add browser selection

--- install.sh
+++ install.sh
@@ -9,9 +9,27 @@ fi
 username=$(id -u -n 1000)
 builddir=$(pwd)
 
+echo "Which browser would you like to install? Please select 1, 2, 3, or 4"
+echo "Options: firefox (1), chromium (2), brave browser (3), google chrome (4)"
+echo "Please note that if you choose 1, 2, or 4, the Super+B shortcut for the browser will not work, so after install you'll have to edit the sxhkdrc file to change to your preferred browser."
+read -rp "Enter browser number: " browser
+browser_choice=$(echo "$browser" | tr '[:upper:]' '[:lower:]')
+
+# Function to install Brave (requires special steps)
+install_brave() {
+    echo "Installing Brave..."
+    sudo nala install curl -y
+    sudo curl -fsSL https://brave.com/signing-key.asc | gpg --dearmor -o /usr/share/keyrings/brave-browser-archive-keyring.gpg
+    echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" \
+        | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+    sudo nala update
+    sudo nala install brave-browser -y
+}
+
 # Update packages list and update system
 apt update
 apt upgrade -y
+
 
 # Install nala
 apt install nala -y
@@ -24,7 +42,7 @@ mkdir -p /home/$username/Pictures
 cp .Xresources /home/$username
 cp .Xnord /home/$username
 cp -R dotconfig/* /home/$username/.config/
-cp bg.jpg /home/$username/Pictures/background.jpg
+cp background.jpg /home/$username/Pictures/background.jpg
 mv user-dirs.dirs /home/$username/.config
 chown -R $username:$username /home/$username
 
@@ -35,9 +53,7 @@ nala install neofetch flameshot psmisc vim lxappearance papirus-icon-theme fonts
 
 # Download Nordic Theme
 cd /usr/share/themes/
-wget https://github.com/EliverLara/Sweet/releases/download/v4.0/Sweet-Dark-v40.zip
-unzip Sweet-Dark-v40.zip
-rm Sweet-Dark-v40.zip
+git clone https://github.com/EliverLara/Nordic.git
 
 # Installing fonts
 cd $builddir 
@@ -61,12 +77,29 @@ cd Nordzy-cursors
 cd $builddir
 rm -rf Nordzy-cursors
 
-# Install brave-browser
-nala install apt-transport-https curl -y
-curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | tee /etc/apt/sources.list.d/brave-browser-release.list
-nala update
-nala install brave-browser -y
+# Install browser of choice
+case "$browser_choice" in
+    1|"firefox")
+        echo "Installing Firefox..."
+        sudo nala install -y firefox
+        ;;
+    2|"chromium")
+        echo "Installing Chromium..."
+        sudo nala install -y chromium
+        ;;
+    3|"brave")
+        install_brave
+        ;;
+    4|"google chrome"|"chrome")
+        wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+        sudo nala install ./google-chrome-stable_current_amd64.deb -y
+        rm -f ./google-chrome-stable_current_amd64.deb
+        ;;
+    *)
+        echo "Unsupported browser: $browser_choice"
+        exit 1
+        ;;
+esac
 
 # Enable graphical login and change target from CLI to GUI
 systemctl enable lightdm
